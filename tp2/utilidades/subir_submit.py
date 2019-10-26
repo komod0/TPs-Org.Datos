944faf6a1aa1c6012a38c85120d73e7c0cdb6f28
komod0 <juliancrespo15@gmail.com>
2019-10-26 16:45:11 -0300

    Fix script para subir submits

--- tp2/utilidades/subir_submit.py
+++ tp2/utilidades/subir_submit.py
@@ -5,16 +5,17 @@ def subir(archivo, mensaje, competicion="Inmuebles24"):
     competicion: nombre de la competicion, por defecto es la del tp
 
     Para poder ejecutar el script se debe tener instalado el modulo kaggle que se puede instalar mediante pip y
-    haberse autenticado generando y ubicado donde corresponda el archivo kaggle.json, instruccion aca: 
-    https://github.com/Kaggle/kaggle-api#api-credential
+    haberse autenticado generando y ubicado donde corresponda el archivo kaggle.json, instruccion aca: \
+            https://github.com/Kaggle/kaggle-api#api-credential
     """
+    msj1 = "Asegurate de tener instalado kaggle crack, sino lo tenes usa algo como: pip install --user kaggle"
+    msj2 = "Te olvidaste de generar el archivo de autenticacion para eso segui las instrucciones en: https://github.com/Kaggle/kaggle-api#api-credential"
     try:
         import kaggle
     except ModuleNotFoundError:
-        print("Asegurate de tener instalado kaggle crack, sino lo tenes usa algo como: pip install --user kaggle")
+        raise ModuleNotFoundError(msj1)
     except OSError:
-        print("Te olvidaste de generar el archivo de autenticacion para eso segui las instrucciones en:\n\
-              https://github.com/Kaggle/kaggle-api#api-credential")
+        raise OSError(msj2)
     sesion = kaggle.KaggleApi()
     sesion.authenticate()
     sesion.competition_submit(f"../submits/{archivo}.csv", mensaje, competicion)
